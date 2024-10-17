4115c0197719b89a1268892796b7f827b8b16e43
Choconaldo <audioafrfb@gmail.com>
2024-10-17 16:38:13 -0300

    adciona o filtro por categoria

--- src/productsList.ts
+++ src/productsList.ts
@@ -3,6 +3,9 @@
 const productsListEl = document.getElementById(
 	"productsList"
 ) as HTMLDivElement;
+const categorySelectEl = document.getElementById(
+	"categorySelect"
+) as HTMLSelectElement;
 
 //* Product inteface
 
@@ -19,9 +22,8 @@ interface Product {
 //* Get all products function
 
 async function getAllProducts() {
-	return fetch("https://fakestoreapi.com/products")
-		.then((response) => response.json())
-		.then((processedResponse) => processedResponse);
+	const products = await fetch("https://fakestoreapi.com/products");
+	return await products.json();
 }
 
 const allProducts = await getAllProducts();
@@ -30,9 +32,10 @@ const allProducts = await getAllProducts();
 //* Get products by ID function
 
 async function getProductById(productId: number) {
-	return fetch(`https://fakestoreapi.com/products/${productId}`)
-		.then((response) => response.json())
-		.then((processedResponse) => processedResponse);
+	const products = await fetch(
+		`https://fakestoreapi.com/products/${productId}`
+	);
+	return await products.json();
 }
 
 // const product = await getProductById(1);
@@ -61,10 +64,34 @@ renderProducts(allProducts);
 //* Get all categories function
 
 async function getAllCategories() {
-	return fetch("https://fakestoreapi.com/products/categories")
-		.then((response) => response.json())
-		.then((processedResponse) => processedResponse);
+	const categories = await fetch(
+		"https://fakestoreapi.com/products/categories"
+	);
+	return await categories.json();
 }
 
-const allCategories = await getAllCategories();
-console.log(allCategories);
+// const allCategories = await getAllCategories();
+// console.log(allCategories);
+
+//* Render categories option function
+
+async function renderCategoriesFilter() {
+	const categoriesToRender = await getAllCategories();
+	let renderedCategoriesOptions = `<option selected disabled></option>`;
+	categoriesToRender.forEach((category: string) => {
+		renderedCategoriesOptions += `<option value="${category}">${category}</option>`;
+		categorySelectEl.innerHTML = renderedCategoriesOptions;
+	});
+}
+
+renderCategoriesFilter();
+
+//* filter by catgory function
+
+categorySelectEl.addEventListener("change", function filterByCategory() {
+	const selectedCategory = categorySelectEl.value;
+	const filteredCatecory = allProducts.filter((product: Product) => {
+		return product.category === selectedCategory;
+	});
+	renderProducts(filteredCatecory);
+});
